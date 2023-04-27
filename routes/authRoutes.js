adc7adc57c2dea73f62c124542e22099216d6808
Rocketeer <68785035+Artgeik@users.noreply.github.com>
2023-04-27 05:41:42 +0530

    decreased length of validator

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,12 +7,12 @@ const auth = require("../middleware/auth");
 
 const registerSchema= Joi.object({
     username:Joi.string().min(3).max(12).required(),
-    password:Joi.string().min(6).max(12).required(),
+    password:Joi.string().min(3).max(12).required(),
     mail:Joi.string().email().required(),
 });
 
 const loginSchema = Joi.object({
-    password:Joi.string().min(6).max(12),
+    password:Joi.string().min(3).max(12),
     mail:Joi.string().email()
 })
 
